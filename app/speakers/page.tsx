a634b756ecbc03aec252bb5962ac9133c7fcc7f6
Jordan Lee <jtlee@rmgj2.com>
2024-01-31 18:30:21 -0600

    Added a speaker

--- app/speakers/page.tsx
+++ app/speakers/page.tsx
@@ -23,7 +23,7 @@ export default function Speakers() {
       <h1 className="h1 mt-[12rem]">Speakers</h1>
       <div className="mt-[4rem] flex items-center">
         <Carousel className=" flex items-center">
-          <CarouselContent>
+          <CarouselContent className="max-w-xl">
             {/* Copy and Paste Carousel item to add more items */}
             <CarouselItem className="">
               <div className="p-1">
@@ -73,6 +73,56 @@ export default function Speakers() {
               </div>
             </CarouselItem>{" "}
             {/* Copy and Paste Carousel item to add more items */}
+            <CarouselItem className="">
+              <div className="p-1">
+                <Card className="bg-opacity-15 flex">
+                  <CardContent className="flex flex-col text-center items-center p-6 bg-opacity-15 text-white max-w-xl">
+                    <Image
+                      src={"/bcs.gif"}
+                      height={300}
+                      width={300}
+                      alt="better call saul"
+                      className="rounded-lg"
+                    />
+
+                    <span
+                      className={twMerge("text-lg", inclusiveSans.className)}
+                    >
+                      Slippin' Jimmy
+                    </span>
+
+                    <span
+                      className={twMerge(
+                        "text-lg text-gray-300",
+                        inclusiveSans.className
+                      )}
+                    >
+                      Lawyer Extraordaniare
+                    </span>
+
+                    <span
+                      className={twMerge("text-md", inclusiveSans.className)}
+                    >
+                      Saul Goodman, whose real name is James McGill, is a
+                      fictional character in the television series "Breaking
+                      Bad" and its prequel "Better Call Saul." Created by Vince
+                      Gilligan, Saul is portrayed by actor Bob Odenkirk. Saul is
+                      a criminal lawyer known for his colorful personality,
+                      flamboyant advertisements, and morally flexible approach
+                      to legal representation. In "Better Call Saul," the series
+                      explores Saul's transformation from a struggling public
+                      defender named Jimmy McGill into the morally ambiguous
+                      lawyer viewers came to know in "Breaking Bad." He is
+                      characterized by his witty and fast-talking demeanor, as
+                      well as his willingness to bend or break the law to serve
+                      his clients' interests. Despite his questionable ethics,
+                      Saul became a fan-favorite character for his humorous and
+                      resourceful nature in navigating the criminal underworld.
+                    </span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>{" "}
           </CarouselContent>
           <CarouselPrevious />
           <CarouselNext />
