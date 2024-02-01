67396073fc26af2be6810abdf2f19e4c9aaf0617
Jordan Lee <jtlee@rmgj2.com>
2024-02-01 00:31:33 -0600

    Update ScheduleTable component

--- components/ScheduleTable.tsx
+++ components/ScheduleTable.tsx
@@ -18,19 +18,33 @@ const ScheduleTable = () => {
       <thead className="text-white mb-10">
         <tr>
           <th className="px-8 w-[10%]"></th>
-          <th className="h3 px-16 pb-10">Saturday</th>
-          <th className="h3 px-24 pb-10">Sunday</th>
+          <th className="h3 px-28 pb-10"></th>
+          <th className="h3 px-28 pb-10"></th>
+          <th className="h3 px-28 pb-10"></th>
+          <th className="h3 px-28 pb-10"></th>
           <th className="px-8 w-[10%]"></th>
         </tr>
       </thead>
       <tbody className="divide-y text-white pt-5 text-opacity-80 p-5">
+        <tr className="text-center text-white">
+          <td></td>
+          <td colSpan={2} className="h3 pb-10">
+            Saturday
+          </td>
+          <td colSpan={2} className="h3 pb-10">
+            Sunday
+          </td>
+          <td></td>
+        </tr>
         <tr className="text-center h-12 pt-10">
           <td>
             <span className=" text-opacity-40">8 AM</span>
           </td>
-          <td></td>
+          <td>Placeholder</td>
+          <td>Placeholder</td>
           <td
             rowSpan={2}
+            colSpan={2}
             className=" bg-yellow-100 text-black w-full rounded-[1rem]"
           >
             <TableRowWithModal
@@ -42,105 +56,160 @@ const ScheduleTable = () => {
           </td>
           <td>8 AM</td>
         </tr>
+
         <tr className="text-center h-12">
           <td>9 AM</td>
-          <td rowSpan={1} className=" bg-purple-200 rounded-[1rem] text-black">
-            <TableRowWithModal
-              title={"Check In"}
-              time={"9 AM - 11 AM"}
-              loc={"Front Desk"}
-              desc={"Check In with Auburn Hacks Staff"}
-            />
-          </td>
-          {/* No 3rd td because the row above (breakfast arrives) takes up 2 rows*/}
+          <td>Placeholder</td>
+          <td>Placeholder</td>
           <td>9 AM</td>
         </tr>
         <tr className="text-center  h-12">
           <td>10 AM</td>
           <td>Placeholder</td>
           <td>Placeholder</td>
+          <td>Placeholder</td>
+          <td>Placeholder</td>
           <td>10 AM</td>
         </tr>
         <tr className="text-center h-12">
           <td>11 AM</td>
+          <td
+            rowSpan={1}
+            colSpan={1}
+            className=" bg-yellow-100 text-black w-full rounded-[1rem]"
+          >
+            <TableRowWithModal
+              title={"Check-In"}
+              time={"11 AM - 12 PM"}
+              loc={"Check-In Desk"}
+              desc={"Check-in and get ready!"}
+            />
+          </td>
+          <td
+            rowSpan={2}
+            colSpan={1}
+            className=" bg-red-100 text-black w-full rounded-[1rem]"
+          >
+            <TableRowWithModal
+              title={"Mocktail hour"}
+              time={"11 AM - 1 PM"}
+              loc={"placeHolder"}
+              desc={"Enjoy a refreshing mocktail and meet other attendees!"}
+            />
+          </td>
           <td>Placeholder</td>
           <td>Placeholder</td>
           <td>11 AM</td>
         </tr>
         <tr className="text-center h-12">
           <td>12 PM</td>
+          <td>Placeholder</td>
           <td>Placeholder</td>
           <td>Placeholder</td>
           <td>12 PM</td>
         </tr>
         <tr className="text-center h-12">
           <td>1 PM</td>
+          <td
+            rowSpan={1}
+            colSpan={2}
+            className=" bg-purple-100 text-black w-full rounded-[1rem]"
+          >
+            <TableRowWithModal
+              title={"Hacking Begins!"}
+              time={"1 PM Saturday - 12 PM Sunday"}
+              loc={""}
+              desc={"Enjoy a refreshing mocktail and meet other attendees!"}
+            />
+          </td>
           <td>Placeholder</td>
           <td>Placeholder</td>
           <td>1 PM</td>
         </tr>
         <tr className="text-center  h-12">
           <td>2 PM</td>
+          <td>Placeholder</td>
+          <td>Placeholder</td>
           <td>Placeholder</td>
           <td>Placeholder</td>
           <td>2 PM</td>
         </tr>
         <tr className="text-center h-12">
           <td>3 PM</td>
+          <td>Placeholder</td>
+          <td>Placeholder</td>
           <td>Placeholder</td>
           <td>Placeholder</td>
           <td>3 PM</td>
         </tr>
         <tr className="text-center h-12">
           <td>4 PM</td>
+          <td>Placeholder</td>
+          <td>Placeholder</td>
           <td>Placeholder</td>
           <td>Placeholder</td>
           <td>4 PM</td>
         </tr>
         <tr className="text-center h-12">
           <td>5 PM</td>
+          <td>Placeholder</td>
+          <td>Placeholder</td>
           <td>Placeholder</td>
           <td>Placeholder</td>
           <td>5 PM</td>
         </tr>
         <tr className="text-center  h-12">
           <td>6 PM</td>
+          <td>Placeholder</td>
+          <td>Placeholder</td>
           <td>Placeholder</td>
           <td>Placeholder</td>
           <td>6 PM</td>
         </tr>
         <tr className="text-center h-12">
           <td>7 PM</td>
+          <td>Placeholder</td>
+          <td>Placeholder</td>
           <td>Placeholder</td>
           <td>Placeholder</td>
           <td>7 PM</td>
         </tr>
         <tr className="text-center h-12">
           <td>8 PM</td>
+          <td>Placeholder</td>
+          <td>Placeholder</td>
           <td>Placeholder</td>
           <td>Placeholder</td>
           <td>8 PM</td>
         </tr>
         <tr className="text-center h-12">
           <td>9 PM</td>
+          <td>Placeholder</td>
+          <td>Placeholder</td>
           <td>Placeholder</td>
           <td>Placeholder</td>
           <td>9 PM</td>
         </tr>
         <tr className="text-center h-12">
           <td>10 PM</td>
+          <td>Placeholder</td>
+          <td>Placeholder</td>
           <td>Placeholder</td>
           <td>Placeholder</td>
           <td>10 PM</td>
         </tr>
         <tr className="text-center h-12">
           <td>11 PM</td>
+          <td>Placeholder</td>
+          <td>Placeholder</td>
           <td>Placeholder</td>
           <td>Placeholder</td>
           <td>11 PM</td>
         </tr>
         <tr className="text-center h-12">
           <td>12 AM</td>
+          <td>Placeholder</td>
+          <td>Placeholder</td>
           <td>Placeholder</td>
           <td>Placeholder</td>
           <td>12 AM</td>
